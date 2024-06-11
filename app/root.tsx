ef42dd84c61d8624267f307b7973aa2bccac139c
jbs-fm <36034436+jbs-fm@users.noreply.github.com>
2024-06-11 14:29:56 +0100

    install thirdweb + use ThirdwebProvider

--- app/root.tsx
+++ app/root.tsx
@@ -5,6 +5,7 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+import { ThirdwebProvider } from "thirdweb/react";
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
@@ -16,7 +17,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Links />
       </head>
       <body>
-        {children}
+        <ThirdwebProvider>{children}</ThirdwebProvider>
         <ScrollRestoration />
         <Scripts />
       </body>
